a7d3b21b40acb41fe0ee073395387c14e8f168ee
Djery M'BEYECANATABE <mbeyecanatabed@gmail.com>
2024-11-05 23:51:05 +0000

    Base project coren discovery and gateway

---
+++ seni-backend/api-core/src/main/java/fr/app/seni/core/ApiCore.java
@@ -0,0 +1,5 @@
+package fr.app.seni.core;
+
+public class ApiCore {
+    public static void main(String[] args) {System.out.println("Hello world!");}
+}
---
+++ seni-backend/gateway-server/src/main/java/fr/app/seni/gateway/GatewayServer.java
@@ -0,0 +1,10 @@
+package fr.app.seni.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServer {
+    public static void main(String[] args) {SpringApplication.run(GatewayServer.class, args);}
+}
+
