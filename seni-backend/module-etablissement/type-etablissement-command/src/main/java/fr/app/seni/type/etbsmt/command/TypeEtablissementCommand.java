69f500069f00863029825041a64598c583235904
Djery M'BEYECANATABE <mbeyecanatabed@gmail.com>
2024-11-11 22:39:01 +0000

    Type etablissement command init

---
+++ seni-backend/module-etablissement/type-etablissement-command/src/main/java/fr/app/seni/type/etbsmt/command/TypeEtablissementCommand.java
@@ -0,0 +1,14 @@
+package fr.app.seni.type.etbsmt.command;
+
+import fr.app.seni.core.config.AxonConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({ AxonConfig.class })
+public class TypeEtablissementCommand {
+
+    public static void main(String[] args) {SpringApplication.run(TypeEtablissementCommand.class, args);}
+
+}
